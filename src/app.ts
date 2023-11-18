3da204aca417e3a1f568a0f6c712f4ccdf806a20
Sharif Hasan <hs.sharif819@gmail.com>
2023-11-19 02:16:06 +0600

    add script on package.json

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-
-  res.send(a);
+  res.send('Hello !!!!!!!!!!! I am running');
 });
 
 export default app;
