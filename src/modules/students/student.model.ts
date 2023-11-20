4b105e0343c3d0362cd003ce779b5005ef40178a
Sharif Hasan <hs.sharif819@gmail.com>
2023-11-21 01:08:37 +0600

    students model updated

--- src/modules/students/student.model.ts
+++ src/modules/students/student.model.ts
@@ -69,26 +69,52 @@ const localGuardianSchema = new Schema<LocalGuardian>({
 });
 
 const studentSchema = new Schema<Student>({
-  id: { type: String },
-  name: userNameSchema,
-  gender: { type: String, enum: ['Male', 'Female', 'Others'], required: true },
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: userNameSchema,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['Male', 'Female', 'Others'],
+      message: '{VALUE} is not a valid gender',
+    },
+    required: true,
+  },
   dateOfBirth: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   contactNo: { type: String, required: true },
   emergencyContact: { type: String, required: true },
   bloodGroup: {
     type: String,
-    enum: ['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'],
-    required: true,
+    enum: {
+      values: ['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'],
+      message: 'Please provide your blood group',
+    },
   },
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: guardinSchema,
-  localGuardian: localGuardianSchema,
+  guardian: {
+    type: guardinSchema,
+    required: true,
+  },
+  localGuardian: {
+    type: localGuardianSchema,
+    required: true,
+  },
   profilePicture: {
     type: String,
   },
-  isActive: { type: String, enum: ['Active', 'Block'], required: true },
+  isActive: {
+    type: String,
+    enum: ['Active', 'Block'],
+    default: 'Active',
+  },
 });
 
 //model
